88d407270f8be20b2ff01a0a8971d2e913dfb2bc
Julieta <cipjuli7@gmail.com>
2026-08-24 12:51:39 -0300

    footer y parte de la tabla socio

---
+++ src/app/ayuda/ayuda.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ayuda',
+  imports: [],
+  templateUrl: './ayuda.html',
+  styleUrl: './ayuda.css',
+})
+export class Ayuda {}
