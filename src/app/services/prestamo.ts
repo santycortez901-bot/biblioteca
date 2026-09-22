e2227e2e05488db793441c7042edf5b072c8f6d8
lucio12121212 <luciosebastian12345@gmail.com>
2026-09-22 08:28:25 -0300

    prestamos terminado

--- src/app/services/prestamo.ts
+++ src/app/services/prestamo.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Prestamo } from '../models/models/prestamo';
 import { LibroService } from './libro-service';
 
@@ -9,37 +9,61 @@ export class PrestamoService {
 
   private prestamos: Prestamo[] = [];
 
+  constructor(
+    private libroService: LibroService
+  ) {}
+
   obtenerPrestamos(): Prestamo[] {
     return this.prestamos;
   }
 
   agregarPrestamo(nuevoPrestamo: Prestamo & { libroId?: string }): boolean {
-    const libroId = nuevoPrestamo.libroId || this.libroService.libros.find(
-      l => l.titulo.toLowerCase() === nuevoPrestamo.libro.toLowerCase()
-    )?.id || '';
+
+    const libroId =
+      nuevoPrestamo.libroId ||
+      this.libroService.libros.find(
+        l =>
+          l.titulo.toLowerCase() ===
+          nuevoPrestamo.libro.toLowerCase()
+      )?.id ||
+      '';
 
     // Modifica automáticamente la copia física a 'Prestada'
-    const exito = this.libroService.prestarCopia(libroId, nuevoPrestamo.inventario);
-    
+    const exito = this.libroService.prestarCopia(
+      libroId,
+      nuevoPrestamo.inventario
+    );
+
     if (exito) {
       this.prestamos.push(nuevoPrestamo);
       return true;
     }
+
     return false;
   }
 
   devolverPrestamo(id: string): void {
-    const prestamo = this.prestamos.find(p => p.id === id);
+
+    const prestamo = this.prestamos.find(
+      p => p.id === id
+    );
 
     if (prestamo) {
+
       prestamo.estado = 'devuelto';
+
       // Devuelve automáticamente la copia física a 'Disponible'
-      this.libroService.devolverCopiaPorInventario(prestamo.inventario);
+      this.libroService.devolverCopiaPorInventario(
+        prestamo.inventario
+      );
     }
   }
 
   renovarPrestamo(id: string): void {
-    const prestamo = this.prestamos.find(p => p.id === id);
+
+    const prestamo = this.prestamos.find(
+      p => p.id === id
+    );
 
     if (prestamo && prestamo.renovaciones < 2) {
 
@@ -63,15 +87,15 @@ export class PrestamoService {
     motivo: string
   ): void {
 
-    const prestamo =
-      this.prestamos.find(p => p.id === id);
+    const prestamo = this.prestamos.find(
+      p => p.id === id
+    );
 
     if (prestamo) {
 
       prestamo.estado = 'suspendido';
 
       prestamo.motivoSuspension = motivo;
-
     }
   }
 }
